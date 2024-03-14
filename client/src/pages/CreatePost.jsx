96f6c54ac474f1db91ff83804b9fe3357c9f5fbc
eazynesta <nestaamwangi@gmail.com>
2024-03-14 18:52:59 +0300

    post page

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import 'react-quill/dist/quill.snow.css';
+
 function CreatePost(){
+    const[title,setTitle] = useState('');
+    const[summary,setSummary] = useState('');
     return(
-        <div>
-            <p>Hello There</p>
-        </div>
+        <form>
+          <input type="title" placeholder="Title" /> 
+          <input type="summary" placeholder="Summary" /> 
+          <input type="file"/>
+          <ReactQuill/>
+          <button style={{marginTop:'5px'}}>Create Post</button>
+        </form>
     );
 
 }
