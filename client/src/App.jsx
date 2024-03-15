702441ade579a5c84a6099eeb3b6a18ee1e4dbfc
eazynesta <nestaamwangi@gmail.com>
2024-03-15 12:38:26 +0300

    post page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import LoginPage from './pages/LoginPage.jsx';
 import RegisterPage from './pages/RegisterPage.jsx';
 import { UserContextProvider } from './UserContext.jsx';
 import CreatePost from './pages/CreatePost.jsx';
+import PostPage from './pages/PostPage.jsx';
 function App() {
   return (
     <>
@@ -19,6 +20,7 @@ function App() {
             <Route path='/login' element={<LoginPage/>}/>
             <Route path='/register' element={<RegisterPage/>}/>
             <Route path='/create' element={<CreatePost/>}/>
+            <Route path="/post/:id" element={<PostPage/>}/>
           </Route>        
         </Routes>
     </UserContextProvider>
